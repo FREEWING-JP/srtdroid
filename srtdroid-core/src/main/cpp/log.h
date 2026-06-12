94a8f544fe95ba801d38d0bee212fb70f0a2f1b5
FREEWING.JP <freewing.jp@gmail.com>
2026-06-12 20:59:57 +0900

    Update log.h

--- srtdroid-core/src/main/cpp/log.h
+++ srtdroid-core/src/main/cpp/log.h
@@ -27,6 +27,7 @@ extern "C" {
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+
 #ifdef __cplusplus
 }
 #endif
